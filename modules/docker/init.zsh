e56ffeff36f37361b6757cbe60230106a833d5c3
Ian Duggan <ian@ianduggan.net>
2017-04-23 15:11:59 -0700

    docker utils

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -1,5 +1,5 @@
 hash boot2docker 2>/dev/null && {
-	if [ "$(boot2docker status)" = "running" ] ; then
+	if boot2docker status 2>&/dev/null && [ "$(boot2docker status)" = "running" ] ; then
 	    $(boot2docker shellinit)
 	fi
 }
@@ -13,6 +13,28 @@ hash docker-machine 2>/dev/null && {
 alias ddm=docker-machine
 alias ddc=docker-compose
 
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
-dexec() { docker exec -it $(docker ps -aqf "name=$1") "$2"; }
-dips() { docker ps -q | xargs -n 1 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }}' }
+dspec() {
+	  spec=$1 ; shift
+	  id=$(docker ps -aqf "name=$spec")
+		echo ${id:-$spec}
+}
+
+dbash() {
+	  spec=$1 ; shift
+		docker exec -it $(dspec $spec) bash;
+}
+
+dexec() {
+	  spec=$1 ; shift
+	  docker exec -it $(dspec $spec) "$@";
+}
+
+docker_ips() {
+    docker ps -q | xargs -n 1 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} {{ .Name }} {{ .ID }}'
+}
+
+docker_ip() {
+		spec=$1 ; shift
+		id=$(dspec $spec)
+	  docker_ips | awk "/$id/ {print \$1}"
+}
