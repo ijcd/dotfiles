b8c76fc66db952758f3a208f22a90033d56dc6f8
Ian Duggan <ian@ianduggan.net>
2017-08-23 10:02:15 -0700

    docker config

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -1,32 +1,37 @@
 hash boot2docker 2>/dev/null && {
-	if boot2docker status 2>&/dev/null && [ "$(boot2docker status)" = "running" ] ; then
-	    $(boot2docker shellinit)
-	fi
+    if boot2docker status 2>&/dev/null && [ "$(boot2docker status)" = "running" ] ; then
+        $(boot2docker shellinit)
+    fi
 }
 
 hash docker-machine 2>/dev/null && {
-	if [ "$(docker-machine status default)" = "Running" ] ; then
-		eval "$(docker-machine env default)"
-	fi
+    if [ "$(docker-machine status default)" = "Running" ] ; then
+        eval "$(docker-machine env default)"
+    fi
 }
 
 alias ddm=docker-machine
 alias ddc=docker-compose
 
 dspec() {
-	  spec=$1 ; shift
-	  id=$(docker ps -aqf "name=$spec")
-		echo ${id:-$spec}
+    spec=$1 ; shift
+    id=$(docker ps -aqf "name=$spec")
+    echo ${id:-$spec}
+}
+
+dattach() {
+    spec=$1 ; shift
+    docker attatch $(dspec $spec);
 }
 
 dbash() {
-	  spec=$1 ; shift
-		docker exec -it $(dspec $spec) bash;
+    spec=$1 ; shift
+    docker exec -it $(dspec $spec) bash;
 }
 
 dexec() {
-	  spec=$1 ; shift
-	  docker exec -it $(dspec $spec) "$@";
+    spec=$1 ; shift
+    docker exec -it $(dspec $spec) "$@";
 }
 
 docker_ips() {
@@ -34,7 +39,7 @@ docker_ips() {
 }
 
 docker_ip() {
-		spec=$1 ; shift
-		id=$(dspec $spec)
-	  docker_ips | awk "/$id/ {print \$1}"
+    spec=$1 ; shift
+    id=$(dspec $spec)
+    docker_ips | awk "/$id/ {print \$1}"
 }
