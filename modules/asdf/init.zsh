367bdbb1ae09f8905d8089ce491760ac9c302a4e
Ian Duggan <ian@ianduggan.net>
2023-07-04 13:14:43 -0700

    asdf updates, git diff updates

--- modules/asdf/init.zsh
+++ modules/asdf/init.zsh
@@ -7,3 +7,7 @@
   source /usr/local/opt/asdf/asdf.sh
   source /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
 }
+
+[ -f /usr/local/opt/asdf/libexec/asdf.sh ] && {
+  source /usr/local/opt/asdf/libexec/asdf.sh
+}
