a589abaa073b6308fa1a12e81700e7fc0d31a2a0
Ian Duggan <ian@ianduggan.net>
2023-07-06 15:00:31 -0700

    Update asdf/init.zsh

--- modules/asdf/init.zsh
+++ modules/asdf/init.zsh
@@ -1,12 +1,12 @@
-[ -d $HOME/.asdf ] && {
-  source $HOME/.asdf/asdf.sh
-  source $HOME/.asdf/completions/asdf.bash
-}
+# [ -d $HOME/.asdf ] && {
+#   source $HOME/.asdf/asdf.sh
+#   source $HOME/.asdf/completions/asdf.bash
+# }
 
-[ -d /usr/local/opt/asdf ] && {
-  source /usr/local/opt/asdf/asdf.sh
-  source /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
-}
+# [ -d /usr/local/opt/asdf ] && {
+#   source /usr/local/opt/asdf/asdf.sh
+#   source /usr/local/opt/asdf/etc/bash_completion.d/asdf.bash
+# }
 
 [ -f /usr/local/opt/asdf/libexec/asdf.sh ] && {
   source /usr/local/opt/asdf/libexec/asdf.sh
