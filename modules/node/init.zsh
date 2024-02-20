cb007b94b0db07d22f2eec98f9d2bb987d06c39a
Ian Duggan <ian@ianduggan.net>
2024-02-19 17:03:54 -0800

    git config stuff mostly

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -1,3 +1,7 @@
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+# mkdir -p $NVM_DIR
+
+# [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+# [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+source $(brew --prefix nvm)/nvm.sh
