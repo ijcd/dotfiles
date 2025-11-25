7eaa0c3d5d86a33f8bd9d2cabc7cb2912a2b43f8
Ian Duggan <ian@ianduggan.net>
2025-11-25 14:08:56 -0800

    update nix/init.zsh

--- modules/nix/init.zsh
+++ modules/nix/init.zsh
@@ -1,2 +1,2 @@
-[ -f ~/.nix-profile/etc/profile.d/nix.sh ] && source ~/.nix-profile/etc/profile.d/nix.sh
+# [ -f ~/.nix-profile/etc/profile.d/nix.sh ] && source ~/.nix-profile/etc/profile.d/nix.sh
 # if [ -e /Users/ijcd/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/ijcd/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
