2aad060e3d81f732ef18e60747b7746ce805bbbe
Ian Duggan <ian@ianduggan.net>
2026-03-31 01:59:10 -0700

    devshell: add java template

---
+++ dot_config/devshell-templates/java/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Java dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              jdk
+              maven
+              gradle
+            ];
+
+            shellHook = ''
+              export JAVA_HOME="${pkgs.jdk}"
+            '';
+          };
+        });
+    };
+}
