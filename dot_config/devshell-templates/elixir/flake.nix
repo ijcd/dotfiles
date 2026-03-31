374889d2da21e96b21ac61dc8fd29b9b17524195
Ian Duggan <ian@ianduggan.net>
2026-03-31 01:59:08 -0700

    devshell: add elixir template

---
+++ dot_config/devshell-templates/elixir/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Elixir dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              elixir
+              erlang
+              hex
+              rebar3
+              postgresql
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              inotify-tools
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.CoreServices
+              darwin.apple_sdk.frameworks.CoreFoundation
+            ];
+
+            shellHook = ''
+              export LANG=en_US.UTF-8
+              export ERL_AFLAGS="-kernel shell_history enabled"
+              export MIX_HOME="$PWD/.mix"
+              export HEX_HOME="$PWD/.hex"
+              mkdir -p "$MIX_HOME" "$HEX_HOME"
+            '';
+          };
+        });
+    };
+}
