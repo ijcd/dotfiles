b296542842b617d1312ff0a7afeae6fbf26e94bc
Ian Duggan <ian@ianduggan.net>
2026-04-09 02:32:49 -0700

    home-manager: symlink Nix GUI apps to ~/Applications
    Spotlight/Alfred need apps in ~/Applications to index them.
    Links all .app bundles from Home Manager profile on activation.

--- dot_config/nix/common/default.nix
+++ dot_config/nix/common/default.nix
@@ -1,4 +1,4 @@
-{ primaryUser, ... }:
+{ primaryUser, lib, ... }:
 {
   imports = [
     ./packages.nix
@@ -24,5 +24,15 @@
 
     # create .hushlogin file to suppress login messages
     file.".hushlogin".text = "";
+
+    # Symlink Nix GUI apps to ~/Applications so Spotlight/Alfred finds them
+    activation.linkNixApps = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      app_folder="$HOME/Applications/Home Manager Apps"
+      mkdir -p "$app_folder"
+      find "$genProfilePath/home-path/Applications" -name "*.app" -maxdepth 1 -print0 2>/dev/null | while IFS= read -r -d "" app; do
+        app_name=$(basename "$app")
+        $DRY_RUN_CMD ln -sf "$app" "$app_folder/$app_name"
+      done
+    '';
   };
 }
